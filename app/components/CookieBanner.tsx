74bb51f0f9e666075477cf9b10388b8c714ad88f
Damian Larenas <dami028@hotmail.com.ar>
2023-11-14 00:52:52 -0300

    GA4 cookie banner fix

--- app/components/CookieBanner.tsx
+++ app/components/CookieBanner.tsx
@@ -1,50 +1,52 @@
 'use client';
 
-import Link from 'next/link'
-import { getLocalStorage, setLocalStorage } from '@/app/lib/storageHelper';
+import { pageview } from '@/app/lib/gTagHelper';
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { getLocalStorage, setLocalStorage } from '@/app/lib/storageHelper';
 
-export default function CookieBanner(){
-    const [cookieConsent, setCookieConsent] = useState(false);
+export default function CookieBanner() {
+  const [cookieConsent, setCookieConsent] = useState(false);
 
-    useEffect (() => {
-        const storedCookieConsent = getLocalStorage("cookie_consent", null)
+  useEffect(() => {
+    const storedCookieConsent = getLocalStorage("cookie_consent", null);
+    setCookieConsent(storedCookieConsent);
+  }, [setCookieConsent]);
 
-        setCookieConsent(storedCookieConsent)
-    }, [setCookieConsent])
+  useEffect(() => {
+    const newValue = cookieConsent ? 'granted' : 'denied';
 
-    
-    useEffect(() => {
-        const newValue = cookieConsent ? 'granted' : 'denied'
+    // Utiliza 'gtag' con el evento 'consent' y la información adecuada
+    pageview('GA_MEASUREMENT_ID', '/cookie-consent'); // Ajusta 'GA_MEASUREMENT_ID' y la ruta según tu configuración
 
-        window.gtag("consent", 'update', {
-            'analytics_storage': newValue
-        });
+    // Elimina 'ad_storage' y 'analytics_storage' del objeto que se pasa a 'window.gtag'
+    window.gtag("consent", 'update', {
+      'page_path': '/cookie-consent', // Ajusta la ruta según tu configuración
+    });
 
-        setLocalStorage("cookie_consent", cookieConsent)
+    setLocalStorage("cookie_consent", cookieConsent);
+  }, [cookieConsent]);
 
-        //For Testing
-        console.log("Cookie Consent: ", cookieConsent)
+  return (
+    <div>
+      <div className='text-center'>
+        <Link href="/info/cookies">
+          <p>We use <span>cookies</span> on our site.</p>
+        </Link>
+      </div>
+      <div className='flex gap-2'>
+        <button
+          onClick={() => setCookieConsent(false)}
+        >
+          Decline
+        </button>
 
-    }, [cookieConsent]);
-
-    return (
-        <div>
-            <div className='text-center'>
-                <Link href="/info/cookies"><p>We use <span>cookies</span> on our site.</p></Link>
-            </div>
-            <div className='flex gap-2'>
-                <button
-                    onClick={() => setCookieConsent(false)}
-                >
-                    Decline
-                </button>
-
-                <button
-                    onClick={() => setCookieConsent(true)}
-                >
-                    Allow Cookies
-                </button>
-            </div>   
-        </div>
-    )}
+        <button
+          onClick={() => setCookieConsent(true)}
+        >
+          Allow Cookies
+        </button>
+      </div>
+    </div>
+  );
+}
--- app/lib/gTagHelper.tsx
+++ app/lib/gTagHelper.tsx
@@ -1,5 +1,13 @@
-export const pageview = (GA_MEASUREMENT_ID : string, url : string) => {
+// Agrega esta declaración global antes de tu función pageview
+declare global {
+    interface Window {
+      gtag: (event: string, GA_MEASUREMENT_ID: string, options: { page_path: string }) => void;
+    }
+  }
+  
+  export const pageview = (GA_MEASUREMENT_ID: string, url: string) => {
     window.gtag("config", GA_MEASUREMENT_ID, {
-        page_path: url,
+      page_path: url,
     });
-};
+  };
+  
